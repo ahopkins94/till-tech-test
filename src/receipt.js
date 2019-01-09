4eaf9e8e95fcd5d02b46d7a252a719ec6ff5e0fe
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-09 16:12:42 +0000

    Passed test for a receipt to display the details of a coffeeshop's table's order

--- src/receipt.js
+++ src/receipt.js
@@ -8,4 +8,30 @@ Receipt.prototype.getCalculator = function(amountPaid, calculator = require('../
   this.calculator = new calculator(this.order, amountPaid);
 };
 
+Receipt.prototype.itemsFormat = function() {
+  var items = this.order.items;
+  string = "\n";
+  for (var i = 0; i < items.length; i++) {
+    string += " " + items[i].name + " " + items[i].quantity + " x " + items[i].price + "\n";
+  }
+  return string;
+};
+
+Receipt.prototype.display = function() {
+  return this._dateFormat() + " " + this._timeFormat() + "\n" + this.order.coffeeshop.name + "\n" + this.order.coffeeshop.address + "\n" + this.order.coffeeshop.number + "\n" + this.itemsFormat() + "Tax: " + this.calculator.amountOfTax + "\nTotal: " + this.calculator.totalIncTax + "\nCash: " + this.calculator.amountPaid;
+};
+
+Receipt.prototype._pad = function(number) {
+  return(number < 10 ? '0' : '') + number;
+};
+
+
+Receipt.prototype._dateFormat = function() {
+  return this.today.getDate()+'/'+this.today.getMonth()+'/'+this.today.getFullYear();
+};
+
+Receipt.prototype._timeFormat = function() {
+  return this._pad(this.today.getHours())+':'+this._pad(this.today.getMinutes())+':'+this._pad(this.today.getSeconds());
+};
+
 module.exports = Receipt;
