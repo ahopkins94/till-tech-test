63a4924c1465ae669a72ebeeb4c2fa7f02760828
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-09 14:04:27 +0000

    Written test for an order to not add an item to items it it is not on the coffeeshop's items list

--- spec/order-spec.js
+++ spec/order-spec.js
@@ -9,5 +9,13 @@ describe("Order", function() {
     order.addItem("Cafe Latte", 4.75, 2);
     expect(order.items.length).toEqual(1);
   });
+
+  it("should not add an item to items if it is not on the coffeeshop's prices list", function() {
+    coffeeshop = jasmine.createSpyObj('coffeeshop', ['prices']);
+    coffeeshop.prices = ["Cafe Latte: " + 4.75];
+    order = new Order(coffeeshop);
+    order.addItem("chips", 4.75, 2);
+    expect(order.items.length).toEqual(0);
+  });
   
 });
