819efa5bf0b01c46707ef2cd09c5dd43e436584d
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-09 14:05:20 +0000

    Written test for an order to not add an item to items if the price does not match the coffeeshop's items

--- spec/order-spec.js
+++ spec/order-spec.js
@@ -17,5 +17,13 @@ describe("Order", function() {
     order.addItem("chips", 4.75, 2);
     expect(order.items.length).toEqual(0);
   });
+
+  it("should not add an item to items if it is not on the coffeeshop's prices list", function() {
+    coffeeshop = jasmine.createSpyObj('coffeeshop', ['prices']);
+    coffeeshop.prices = ["Cafe Latte: " + 4.75];
+    order = new Order(coffeeshop);
+    order.addItem("Cafe Latte", 4, 2);
+    expect(order.items.length).toEqual(0);
+  });
   
 });
