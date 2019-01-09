7961c21dd47c6189620434dee1587f5c6e528e8a
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-09 14:06:45 +0000

    Written test for a calculator to have a constant tax

---
+++ spec/calculator-spec.js
@@ -0,0 +1,21 @@
+describe("Calculator", function() {
+  var Calculator = require('../src/calculator.js')
+  var calculator;
+
+  beforeEach(function() {
+    item = jasmine.createSpyObj('item', ['price', 'quantity']);
+    item.price = 3.5;
+    item.quantity = 2;
+    item2 = jasmine.createSpyObj('item', ['price', 'quantity']);
+    item2.price = 4;
+    item2.quantity = 1;
+    order = jasmine.createSpyObj('order', ['items']);
+    order.items = [item, item2];
+    calculator = new Calculator(order, 15);
+  });
+
+  it("should have a constant tax", function() {
+    expect(calculator.TAX).toEqual(0.2);
+  });
+
+});
