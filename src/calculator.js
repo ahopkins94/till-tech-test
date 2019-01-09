f1bff3c971d0a23e33b49dddbc2d88430476e598
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-09 14:09:42 +0000

    Passing test for a calculator to have a total

--- src/calculator.js
+++ src/calculator.js
@@ -4,4 +4,13 @@ function Calculator(order, amountPaid) {
   this.TAX = 0.2;
 }
 
+Calculator.prototype.total = function() {
+  var total = 0;
+  var items = this.order.items;
+  for (var i = 0; i < items.length; i++) {
+    total += (items[i].price * items[i].quantity);
+  }
+  return total;
+};
+
 module.exports = Calculator;
